d6fb3bf3e779ab355335608d28e89017f0a4718d
pazho <kochi.bk@googlemail.com>
2025-11-05 11:37:46 -0500

    A: black screen closing

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+from constants import * 
+
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print("Screen width:" ,SCREEN_WIDTH)
+    print("Screen height:" ,SCREEN_HEIGHT)
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        pygame.Surface.fill(screen,"black")
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+
+
+
+
+
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
